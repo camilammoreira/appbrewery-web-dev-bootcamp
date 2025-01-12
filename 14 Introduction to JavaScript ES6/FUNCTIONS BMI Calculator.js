1c14e10a3c070e5e15029e2f78e6c26cc5660841
camila.moreira <camilammoreira1@gmail.com>
2025-01-12 19:32:32 -0300

    condiditon statements (if else), comparators and combining comparators

--- 14 Introduction to JavaScript ES6/FUNCTIONS BMI Calculator.js
+++ 14 Introduction to JavaScript ES6/FUNCTIONS BMI Calculator.js
@@ -1,8 +1,25 @@
 //BMI Calculator
-function bmiCalculator(weight, height) {
-    var bmiResult = Math.round(weight / Math.pow(height,2); // The ** operator is recent and doesn't work in IE
-    return bmiResult;
+
+// function bmiCalculator(weight, height) {
+//     var bmiResult = Math.round(weight / Math.pow(height,2); // The ** operator is recent and doesn't work in IE
+//     return bmiResult;
+// }
+
+// var bmi = bmiCalculator(56, 1.8);
+// console.log(bmi);
+
+//CONDIDITON STATEMENTS (IF ELSE), COMPARATORS AND COMBINING COMPARATORS
+function bmiCalculator (weight, height) {
+    var bmi = Math.round(weight / Math.pow(height, 2));
+    var interpretation = "";
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight."
+    } else if (bmi >= 18.5 && bmi < 24.9){
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight."
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight."
+    }
+    return interpretation;
 }
 
-var bmi = bmiCalculator(56, 1.8);
-console.log(bmi);
+bmiCalculator(500, 1.8);
--- 14 Introduction to JavaScript ES6/FUNCTIONS Love Calculator.js
+++ 14 Introduction to JavaScript ES6/FUNCTIONS Love Calculator.js
@@ -1,9 +1,8 @@
 // Love  Calculator
 
-// var name1 = prompt("1st name:");
-// var name2 = prompt("2nd name:");
-
-// var n = Math.random() * 100;
-// n = Math.floor(n) + 1;
+var name1 = prompt("1st name:");
+var name2 = prompt("2nd name:");
+var n = Math.random() * 100;
+n = Math.floor(n) + 1;
 
 alert("The results of the calculations by Dr. Love are:\n" + n + "% of compatibility between " + name1 + " and " + name2);
