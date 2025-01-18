170ad3390f01a7b24e72720bce9fb0317991a5dc
camila.moreira <camilammoreira1@gmail.com>
2025-01-18 01:20:22 -0300

    course project gallery page

--- 16.2 DOM Challenge Starting Files/index.js
+++ 16.2 DOM Challenge Starting Files/index.js
@@ -1,3 +1,5 @@
 document.querySelector("ul").lastElementChild.innerText = "Camila";
 
 document.querySelector("li a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
