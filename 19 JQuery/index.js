3b361456ca03caff1e1d80f55c01758590ae6942
camila.moreira <camilammoreira1@gmail.com>
2025-01-24 22:28:54 -0300

    JQuery: Select, manipulate, event listener, add and remove

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -1,2 +1,34 @@
 // $(document).ready(function(){ [code] }) --> same as adding CDN JQuery before index.js
-$("h1").css("color", "red");
+
+// GET style property
+console.log($("h1").css("color"));
+
+// Not good practice - SET style property
+// $("h1").css("color", "yellow");
+
+// Add Class
+$("h1").addClass("tittle");
+
+// Click event listener
+$("h1").click(function() {
+    $("h1").css("color", "purple");
+});
+
+// Event Listener with .on
+$("button").on("click", function () {
+    $("h1").toggleClass("toggle");
+});
+
+// Event Listener with .on
+$(document).on("keydown",function (event) {
+    $("h1").html(event.key);
+})
+
+//  Add element on the fly
+$("h1").before("<button>before</button>");
+$("h1").after("<button>.after</button>");
+$("h1").prepend("<button>.prepend()</button>");
+$("h1").append("<button>.append()</button>");
+
+// Remove element on the fly
+// $("button").remove(); // Removes ALL the buttons because of the $ notation
